bc9bbe4329c7eba439dea7c4d0096a31b8d33b00
Jared Bronen <jbronen@gmail.com>
2015-02-19 13:42:17 -0500

    more work after thursday meeting

---
+++ Flatland/Assets/Scripts/PickupObject.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour {
+
+	bool holdingObject = false;
+	GameObject mainCamera;
+	GameObject carriedObject;
+	public float distance;
+	public float smooth;
+
+	void Start()
+	{
+		mainCamera = GameObject.FindWithTag ("MainCamera");
+	}
+
+	void Update()
+	{
+		if (holdingObject) {
+			carry(carriedObject);
+			if (Input.GetMouseButtonDown (0)) {
+				drop();
+			}
+		} else {
+			pickup();
+		}
+	}
+	
+	void carry(GameObject o)
+	{
+		o.transform.position = Vector3.Lerp (o.transform.position, mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+	}
+
+	void pickup()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			int x = Screen.width / 2;
+			int y = Screen.height / 2;
+
+			Ray ray = mainCamera.camera.ScreenPointToRay(new Vector3(x,y));
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				if (hit.collider.tag == "Pickup") {
+					float distanceToObject = Vector3.Distance(transform.position, hit.transform.position); 
+
+					if (distanceToObject < distance) {
+						holdingObject = true;
+						carriedObject = hit.collider.gameObject;
+						hit.collider.gameObject.rigidbody.useGravity = false;
+						//hit.collider.gameObject.rigidbody.isKinematic = true;
+					}
+				}
+			}
+		}
+
+	}
+
+	void drop()
+	{
+		holdingObject = false;
+		carriedObject.rigidbody.useGravity = true;
+		//carriedObject.rigidbody.isKinematic = false;
+		carriedObject = null;
+
+	}
+}
