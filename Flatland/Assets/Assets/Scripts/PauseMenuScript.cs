4c8daa59be9b23fb4fb899d71e022c6bb4238cf8
Jared Bronen <jbronen@gmail.com>
2015-02-25 13:55:49 -0500

    added dans menu

---
+++ Flatland/Assets/Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuScript : MonoBehaviour {
+
+	public GameObject pauseMenuPanel;
+	public GameObject player;
+
+	private bool isPaused = false;
+
+	void Start () {
+		Time.timeScale = 1;
+		Screen.lockCursor = true;
+	}
+
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Escape) && !isPaused) {
+			PauseGame ();
+		} else if (Input.GetKeyUp (KeyCode.Escape) && isPaused) {
+			UnpauseGame ();
+		}
+	}
+
+	public void PauseGame()
+	{
+		//pause game
+		isPaused = true;
+		Time.timeScale = 0;
+
+		//disable fps controls
+		player.transform.GetComponent<MouseLook> ().enabled = false;
+		Camera.main.GetComponent<MouseLook> ().enabled = false;
+
+		//show cursor
+		Screen.lockCursor = false;
+		Screen.showCursor = true;
+ 
+		//enable pause menu
+		pauseMenuPanel.GetComponent<RectTransform>().position = new Vector3(transform.position.x, 300, transform.position.z);
+	}
+
+	public void UnpauseGame()
+	{
+		isPaused = false;
+		Time.timeScale = 1;
+		//enable fps controls
+		player.transform.GetComponent<MouseLook> ().enabled = true;
+		Camera.main.GetComponent<MouseLook> ().enabled = true;
+
+		//hide cursor
+		Screen.showCursor = false;
+		Screen.lockCursor = true;
+
+		//disable pause menu
+		pauseMenuPanel.GetComponent<RectTransform> ().position = new Vector3 (transform.position.x, 1000, transform.position.z);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+
+	public void RestartLevel()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+}
