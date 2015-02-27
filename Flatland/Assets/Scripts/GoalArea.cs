f3c5c5d658cd89f8d7d57de4a3c506aec0779a8a
bokeef01 <brian.o_keefe@tufts.edu>
2015-02-27 01:33:15 -0500

    Tweaked GoalArea Script

--- Flatland/Assets/Scripts/GoalArea.cs
+++ Flatland/Assets/Scripts/GoalArea.cs
@@ -6,12 +6,19 @@ public class GoalArea : MonoBehaviour {
 	private GameController gameController;
 	private PickupObject solver;
 
-	public Collider goalObject;
+	public GameObject goalObject;
+	Collider goalCollider;
 	public float smooth;
 	public GameObject goal;
 
 	void Start ()
 	{
+		Debug.Log (goalObject.ToString ());
+		if (goalObject.ToString () == "Cube (UnityEngine.GameObject)") {
+			Debug.Log ("It should work.");
+		} else {
+			Debug.Log ("fuck if I know.");
+		}
 		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
 		if (gameControllerObject != null) {
 			gameController = gameControllerObject.GetComponent <GameController> ();
@@ -20,30 +27,37 @@ public class GoalArea : MonoBehaviour {
 			Debug.Log ("Cannot find 'GameController' script");
 		}
 		solver = GameObject.FindWithTag ("Player").GetComponent<PickupObject> ();
+		goalCollider = goalObject.GetComponent<BoxCollider> ();
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other == goalObject) {
-			if (goalObject.ToString() == "FullCube") {
+		Debug.Log (other.ToString ());
+		Debug.Log (goalCollider.ToString ());
+		if (other.ToString() == goalCollider.ToString()) {
+			if (goalObject.ToString() == "CubeSquare (UnityEngine.GameObject)" || goalObject.ToString() == "CubeSquare2 (UnityEngine.GameObject)") {
 				gameController.solvedCube ();
-				if (!solver.holdingObject)
+				if (!solver.holdingObject) {
 					other.transform.position = goal.transform.position; //Vector3.Lerp (other.transform.position,goal.transform.position, Time.deltaTime * smooth);
+				}
 			}
-			else if (goalObject.ToString() == "FullCylinder") {
+			else if (goalObject.ToString() == "CylinderCube (UnityEngine.GameObject)") {
 				gameController.solvedCylinder ();
-				if (!solver.holdingObject)
+				if (!solver.holdingObject) {
 					other.transform.position = goal.transform.position; //Vector3.Lerp (other.transform.position,goal.transform.position, Time.deltaTime * smooth);
+				}
 			}
-			else if (goalObject.ToString() == "Pyramid") {
+			else if (goalObject.ToString() == "PyramidCube (UnityEngine.GameObject)") {
 				gameController.solvedPyramid();
-				if (!solver.holdingObject)
+				if (!solver.holdingObject) {
 					other.transform.position = goal.transform.position; //Vector3.Lerp (other.transform.position,goal.transform.position, Time.deltaTime * smooth);
+				}
 			}
-			//else if (goalObject.ToString() == "Cube") {
+			else if (goalObject.ToString() == "Cube (UnityEngine.GameObject)") {
 				gameController.solvedTutorial();
-			if (!solver.holdingObject)
-				other.transform.position = goal.transform.position; //Vector3.Lerp (other.transform.position,goal.transform.position, Time.deltaTime * smooth);
-			//}
+				if (!solver.holdingObject) {
+					other.transform.position = goal.transform.position; //Vector3.Lerp (other.transform.position,goal.transform.position, Time.deltaTime * smooth);
+				}
+			}
 		}
 	}
 
