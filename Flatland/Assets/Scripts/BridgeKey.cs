e83445a4350beadcb60f2d1103804f71acdb3738
dmh1218 <dmh1218@gmail.com>
2015-03-10 01:16:36 -0400

    added semi-functional bridge to lvl2

---
+++ Flatland/Assets/Scripts/BridgeKey.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeKey : MonoBehaviour {
+
+	public GameObject keyObject;
+	public GameObject bridge;
+	
+	private PickupObject solver;
+	Collider keyCollider;
+	
+	void Start()
+	{
+		solver = GameObject.FindWithTag ("Player").GetComponent<PickupObject> ();
+		keyCollider = keyObject.GetComponent<BoxCollider> ();
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other == keyCollider) {
+			if (bridge.activeSelf == false) {
+				bridge.SetActive (true);
+			}
+		}
+		if (!solver.holdingObject) {
+			//other.transform.position = keyObject.transform.position;
+			other.transform.position = new Vector3 (keyObject.transform.position.x, keyObject.transform.position.y + .1f, keyObject.transform.position.z);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other == keyCollider) {
+			bridge.SetActive (false);
+		}
+	}
+}
