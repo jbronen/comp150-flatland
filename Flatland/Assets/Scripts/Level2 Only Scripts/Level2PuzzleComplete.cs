ecaa857d0515e3dff8bb230f49f25b3f82bfd629
dmh1218 <dmh1218@gmail.com>
2015-03-12 17:44:32 -0400

    level 2 complete

---
+++ Flatland/Assets/Scripts/Level2 Only Scripts/Level2PuzzleComplete.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level2PuzzleComplete : MonoBehaviour {
+
+	private GameController gameController;
+
+	public GameObject step1;
+	public GameObject step2;
+	public GameObject step3;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent <GameController> ();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+	void Update()
+	{
+		if (step1.activeSelf == true) {
+			if (step2.activeSelf == true) {
+				if (step3.activeSelf == true) {
+					gameController.puzzleCompleted();
+				}
+			}
+		}
+	}
+
+
+}
