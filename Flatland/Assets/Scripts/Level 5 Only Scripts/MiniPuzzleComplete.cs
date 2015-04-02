eb06c00c67b14ef0abe82631d0fd4d6557f7e1c0
dmh1218 <dmh1218@gmail.com>
2015-04-02 17:16:23 -0400

    few changes here and there

---
+++ Flatland/Assets/Scripts/Level 5 Only Scripts/MiniPuzzleComplete.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniPuzzleComplete : MonoBehaviour {
+
+	public Material newMaterial;
+	public GameObject rightLight;
+	public GameObject leftLight;
+
+	Renderer rend;
+
+	void Update () {
+		rend = GetComponent<Renderer> ();
+		rend.material = newMaterial;
+	
+	}
+}
