d1537ddcb68b1a1116a2b5a6761514b2a0029fab
dmh1218 <dmh1218@gmail.com>
2015-04-09 19:48:12 -0400

    fixed moving platform slip and lighting level 3

---
+++ Flatland/Assets/Scripts/MoveWithPlatform.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWithPlatform : MonoBehaviour {
+
+	public GameObject originalParent;
+
+	GameObject solver;
+	PickupObject pickupObject;
+
+	void Start()
+	{
+		solver = GameObject.FindGameObjectWithTag ("Player");
+		pickupObject = solver.GetComponent<PickupObject> ();
+	}
+
+	void OnCollisionStay(Collision other)
+	{
+		if (other.gameObject.tag == "Moving Platform") {
+			transform.parent = other.transform;
+		} else {
+			transform.parent = originalParent.transform;
+		}
+	}
+
+//	void OnCollisionExit(Collision other)
+//	{
+//		transform.parent = originalParent.transform;
+//	}
+}
