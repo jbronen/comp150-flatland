762e98f8a602ae5326c5e4359b4ee7e45a870eed
Jared Bronen <jbronen@gmail.com>
2015-04-16 03:28:43 -0400

    level 4 pretty much done

---
+++ Flatland/Assets/Level4PuzzleComplete.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level4PuzzleComplete : MonoBehaviour {
+
+
+	private GameController gameController;
+
+	public GameObject bridge;
+	public GameObject levelPortal;
+
+	// Use this for initialization
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent <GameController> ();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bridge.activeSelf) {
+			gameController.puzzleCompleted ();
+			levelPortal.SetActive(true);
+		}
+	}
+}
