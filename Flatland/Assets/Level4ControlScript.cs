e0499c4c6c4d5987468d3777c10b30ac67728fef
Jared Bronen <jbronen@gmail.com>
2015-05-07 14:04:43 -0400

    level 4 control script working

---
+++ Flatland/Assets/Level4ControlScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level4ControlScript : MonoBehaviour {
+
+	public GameObject square;
+	public GameObject cylinder;
+
+	public GameObject squarePlatform1;
+	public GameObject squarePlatform2;
+	public GameObject cylPlatform1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (square.activeSelf) {
+			squarePlatform1.SetActive (true);
+			squarePlatform2.SetActive (true);
+			cylPlatform1.SetActive (false);
+		} else {
+			squarePlatform1.SetActive (false);
+			squarePlatform2.SetActive (false);
+			cylPlatform1.SetActive (true); 
+		}
+	}
+}
