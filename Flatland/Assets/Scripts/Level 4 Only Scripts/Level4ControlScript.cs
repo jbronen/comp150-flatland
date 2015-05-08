7615336596511d5027440857781e7fd0200fe2ec
Jared Bronen <jbronen@gmail.com>
2015-05-08 10:11:59 -0400

    level2 and level4 updates

--- Flatland/Assets/Scripts/Level 4 Only Scripts/Level4ControlScript.cs
+++ Flatland/Assets/Scripts/Level 4 Only Scripts/Level4ControlScript.cs
@@ -10,6 +10,17 @@ public class Level4ControlScript : MonoBehaviour {
 	public GameObject squarePlatform2;
 	public GameObject cylPlatform1;
 
+	public GameObject movesq1;
+	public GameObject movecyl2;
+	public GameObject movesq2;
+
+	public GameObject sq1;
+	public GameObject cyl1;
+	public GameObject cyl2;
+	public GameObject sq2;
+	public GameObject cyl3;
+	public GameObject sq3;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -21,10 +32,28 @@ public class Level4ControlScript : MonoBehaviour {
 			squarePlatform1.SetActive (true);
 			squarePlatform2.SetActive (true);
 			cylPlatform1.SetActive (false);
+			movesq1.SetActive(true);
+			movesq2.SetActive(true);
+			movecyl2.SetActive(false);
+			sq1.SetActive(true);
+			cyl1.SetActive(false);
+			cyl2.SetActive(false);
+			sq2.SetActive(true);
+			cyl3.SetActive(false);
+			sq3.SetActive(true);
 		} else {
 			squarePlatform1.SetActive (false);
 			squarePlatform2.SetActive (false);
-			cylPlatform1.SetActive (true); 
+			cylPlatform1.SetActive (true);
+			movesq1.SetActive(false);
+			movesq2.SetActive(false);
+			movecyl2.SetActive(true);
+			sq1.SetActive(false);
+			cyl1.SetActive(true);
+			cyl2.SetActive(true);
+			sq2.SetActive(false);
+			cyl3.SetActive(true);
+			sq3.SetActive(false);
 		}
 	}
 }
--- Flatland/Assets/Scripts/Level 4 Only Scripts/Level4PuzzleComplete.cs
+++ Flatland/Assets/Scripts/Level 4 Only Scripts/Level4PuzzleComplete.cs
@@ -6,10 +6,15 @@ public class Level4PuzzleComplete : MonoBehaviour {
 
 	GameController gameController;
 	ObjectAppearTrigger solutionScript;
+	Level4ControlScript control;
 
 	public GameObject solutionPlatform;
+	public GameObject circle1;
+	public GameObject circle2;
+	public GameObject circle3;
 	
 	void Start () {
+		control = GetComponent<Level4ControlScript> ();
 		gameController = GameObject.FindWithTag("GameController").GetComponent <GameController> ();
 		solutionScript = solutionPlatform.GetComponent<ObjectAppearTrigger> ();
 	}
@@ -17,6 +22,10 @@ public class Level4PuzzleComplete : MonoBehaviour {
 	void Update () 
 	{
 		if (solutionScript.isSolved()) {
+			circle1.SetActive(true);
+			circle2.SetActive(true);
+			circle3.SetActive(true);
+			control.enabled = false;
 			gameController.puzzleCompleted ();
 		}
 	}
